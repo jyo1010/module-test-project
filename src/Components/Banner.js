62f6e268ec9dd4583c2426048a51db015dbe736a
jyothika <jyothika1010@gmail.com>
2022-10-02 23:17:40 +0530

    my project

---
+++ src/Components/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='container'>
+            <div className='contents'>
+                <p className='subheading'>SUMMER 2020</p>
+                <h1 className='heading'>NEW COLLECTION</h1>
+                <p className='p1'>We know how large objects will act,</p>
+                <p className='p1'>but things on a small scale.</p>
+                <button className ='btn'>SHOP NOW</button>
+            </div>
+            <div className='image'>
+                <img src='/assets/Girl.png' alt ='no img found' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
+
